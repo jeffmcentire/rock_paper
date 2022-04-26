cc02f91763e6c04f6d5e9c6c0d72b10f2398e247
jeffmcentire <jeffreymcentire@gmail.com>
2022-04-26 16:35:10 -0700

    add structure comments

---
+++ script.js
@@ -0,0 +1,21 @@
+// initialize playerSelection variable
+
+// initialize computerSelection variable
+
+// wait for user input, HTML will set up textbox
+
+// set user input to lowerCase
+
+// check if user input is valid
+
+// return error if playerSelction is invalid
+
+// calculate random computerSelection
+
+// compare playerSelection to computerSelection
+
+// set playerSelection image
+
+// set computerSelection image
+
+// return results of game
