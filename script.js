3cc10837f623c27c51126084a85279585e638cef
jeffmcentire <jeffreymcentire@gmail.com>
2022-04-30 13:52:28 -0700

    html form input to script

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 // initialize playerSelection variable
+//Wait for page to finish loading
+
+//const form = document.getElementById('playerInput');
 
 // initialize computerSelection variable
 
 // wait for user input, HTML will set up textbox
-
+window.addEventListener("load",function(){
+    //Run function when you submit form
+    document.getElementById("form").addEventListener("submit",function(e){
+      //Stop the form from submitting:
+      e.preventDefault();
+  
+      //Get your input value
+      var playerInput = document.getElementById("playerInput").value;
+  
+      //Log it to your console
+      alert(playerInput)
+    });
+  });
 // set user input to lowerCase
 
 // check if user input is valid
